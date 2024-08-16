57ac0a68c50814e76ca8e397b90d7a5ae036eb42
Scarbete <megavertal@gmail.com>
2024-08-16 11:49:19 +0600

    complete lesson five

--- day_five/lesson/lesson.py
+++ day_five/lesson/lesson.py
@@ -1,15 +1,16 @@
 # Alias & import & from & libraries
 from random import randint as generate_random_number
-from termcolor import cprint
-from person import Person
-import calculator
 import emoji
+from decouple import config
+
+from termcolor import cprint
+import calculator
+from person import Person
 
 print(generate_random_number(1, 5))
 
 print(calculator.addition(8, 2))
 print(calculator.multiplication(8, 2))
-
 
 friend = Person(name='MaybeYato', age=20)
 print(f'My friend - {friend}')
@@ -17,3 +18,12 @@ print('Test class', str(friend))
 
 cprint(text='colored text', color='red', on_color='on_cyan')
 print(emoji.emojize('Python is :thumbs_up:'))
+
+db_url = config('DATABASE_URL')
+print(db_url)
+
+number = config('MY_NUMBER', cast=int)
+print(number * 2)
+
+commented_text = config('COMMENTED', default='commented_text')
+print(commented_text)
